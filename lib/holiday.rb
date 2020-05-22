a5a10f1fb7d6817b23a564e3a3a74db0dd532a36
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-22 00:22:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,3 +1,4 @@
+#Hash:
 holiday_supplies = {
   :winter => {:christmas => ["Lights", "Wreath"], :new_years => ["Party Hats"]},
   :summer => {:fourth_of_july => ["Fireworks", "BBQ"]},
@@ -5,17 +6,20 @@ holiday_supplies = {
   :spring => {:memorial_day => ["BBQ"]}
 }
 
+#Method 1:
 def second_supply_for_fourth_of_july(holiday_supplies)
   holiday_supplies[:summer][:fourth_of_july][1]
 end
 second_supply_for_fourth_of_july(holiday_supplies)
 
+#Method 2:
 def add_supply_to_winter_holidays(holiday_hash, item)
  holiday_hash[:winter].each do |holiday, decorations|
    decorations << item
   end
 end
 
+#Method 3: 
 def add_supply_to_winter_holidays(holiday_hash, item)
  holiday_hash[:winter].each do |holiday, decorations|
    decorations.push(item)
