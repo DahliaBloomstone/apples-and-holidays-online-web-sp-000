7c0cae24df678e6cabdfb071aabf06ccdf7d30f5
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-22 00:20:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,7 +1,7 @@
 holiday_supplies = {
   :winter => {:christmas => ["Lights", "Wreath"], :new_years => ["Party Hats"]},
   :summer => {:fourth_of_july => ["Fireworks", "BBQ"]},
-  :fall =>   {:thanksgiving => ["Turkey"]},
+  :fall   => {:thanksgiving => ["Turkey"]},
   :spring => {:memorial_day => ["BBQ"]}
 }
 
